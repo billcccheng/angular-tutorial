40dcbd8adbfb571cae83ae86ed51f77f38c0a9ca
Cheng, Chun-Chan <ccheng@hq.corp.viasat.com>
2017-03-09 16:47:03 -0600

    modified hero property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{title}}</h1>`,
-})
-export class AppComponent  { 
-  title = 'Tours of Heroes';
-  hero = 'WindStorm';
-  name = 'Angular'; 
+export class Hero{
+    id: number;
+    name: string;
 }
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>Hello {{hero.name}}</h1>`,
+})
+
+export class AppComponent { 
+    title = 'Tours of Heroes';
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
+
+
