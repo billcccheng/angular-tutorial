a907e6a988460dd47fc03c0c879c39a81f623280
Cheng, Bill <ccheng@hq.corp.viasat.com>
2017-03-30 13:36:51 -0500

    finished until 'Get heroes'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { FormsModule }    from '@angular/forms';
 import { BrowserModule }  from '@angular/platform-browser';
 import { RouterModule }   from '@angular/router';
 import { AppComponent }        from './app.component';
+import { DashboardComponent }  from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
@@ -15,13 +16,23 @@ import { HeroService }         from './hero.service';
         FormsModule,
         RouterModule.forRoot([
             {
-                path:'heroes',
+                path: '',
+                redirectTo: '/dashboard',
+                pathMatch: 'full'
+            },
+            {
+                path: 'heroes',
                 component: HeroesComponent
+            },
+            {
+                path: 'dashboard',
+                component: DashboardComponent
             }
         ])
     ],
     declarations: [
         AppComponent,
+        DashboardComponent,
         HeroDetailComponent,
         HeroesComponent
     ],
@@ -30,6 +41,7 @@ import { HeroService }         from './hero.service';
     ],
     bootstrap: [ AppComponent ]
 })
+
 export class AppModule {
 }
 
