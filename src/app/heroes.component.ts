90f3f37ebc18d490dfc4a14a43e49bd14c57b418
Bill <iamkg05@gmail.com>
2017-04-04 22:10:48 -0500

    finished routing tutorial

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,41 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-heroes',
-    template:`
-        <h1>{{title}}</h1>
-		<h2>My Heroes</h2>
-		<ul class="heroes">
-          <li *ngFor="let hero of heroes" 
-            (click)="onSelect(hero)"
-            [class.selected]="hero === selectedHero"
-          >
-          <span class="badge">{{hero.id}}</span> {{hero.name}}
-		  </li>
-		</ul>
-        <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-    `,
-    //providers: [HeroService]
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css']
 })
 
 export class HeroesComponent { 
     heroes: Hero[];
     selectedHero: Hero;
+
+    constructor(
+        private heroService: HeroService,
+        private router: Router
+    ) {};
+
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
     }
 
     getHeroes(): void {
-        this.heroes = this.heroService.getHeroes();
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
     }
 
     ngOnInit(): void {
         this.getHeroes();
     }
 
-    constructor(private heroService: HeroService){};
+    gotoDetail(): void{
+        this.router.navigate(['/detail', this.selectedHero.id]);
+    }
+
 }
 
